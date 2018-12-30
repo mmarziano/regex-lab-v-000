ea20c99b7fa46ff97a73b5a00b4230e874271992
Monica Marziano <mmarziano1@gmail.com>
2018-12-30 22:58:09 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+  if word[0].match(/[aeiouAEIOU]/)
+    return true  
+  else 
+    return false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+   text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   !!text.match(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-
+    phone.match(/^[^a-zA-Z]{10}/)
 end
